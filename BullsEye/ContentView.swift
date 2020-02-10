2939b8ee8fdf63898c855d657cb0f884f8c7b000
Wes-White <wesleyrwhite86@gmail.com>
2020-02-10 08:49:21 -1000

    Adding button and pop up, moving to portrait vs landscape

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -11,26 +11,41 @@ import SwiftUI
 struct ContentView: View {
     
     @State var alertIsVisible: Bool = false
+    @State var whoIsThereVisible: Bool = false
     
     var body: some View {
         VStack {
-            
-            Text("Welcome to my first App!")
-                .foregroundColor(Color.green)
-            
-            
             Button(action: {
-                print("Button pressed" )
-                self.alertIsVisible = true
-                })
-            {
-                Text(/*@START_MENU_TOKEN@*/"Hit me!"/*@END_MENU_TOKEN@*/)
+                self.whoIsThereVisible = true
+            }) {
+                
+                Text(/*@START_MENU_TOKEN@*/"Knock Knock.."/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color.red)
             }
-            .alert(isPresented: $alertIsVisible) { () ->
+            .alert(isPresented: $whoIsThereVisible){ () ->
                 Alert in
-                return Alert(title: Text("Hello there!" ), message: Text("This is my first popup"), dismissButton: .default(Text("Awesome!")))
+                return Alert(title: Text("Who's There?" ), message: Text("This is my knock konk joke, I hope you had a lil laugh if not... alls well"), dismissButton: .default(Text("Dismiss")))
             }
-         }
+            VStack {
+                
+                Text("BullsEye")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.green)
+                Button(action: {
+                    print("Button pressed" )
+                    self.alertIsVisible = true
+                    })
+                {
+                    Text(/*@START_MENU_TOKEN@*/"Hit me!"/*@END_MENU_TOKEN@*/)
+                }
+                .alert(isPresented: $alertIsVisible) { () ->
+                    Alert in
+                    return Alert(title: Text("Hello there!" ), message: Text("This is my first popup"), dismissButton: .default(Text("Awesome!")))
+                }
+              
+                          
+            }
+        }
      }
  }
 
