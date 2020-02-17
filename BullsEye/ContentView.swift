0bd5a6bee6959636a3edbf874a09e387fe90f065
Wes-White <wesleyrwhite86@gmail.com>
2020-02-17 09:24:45 -1000

    Changed settings to landscape view

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -9,48 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var alertIsVisible: Bool = false
-    @State var whoIsThereVisible: Bool = false
-    
-    var body: some View {
-        VStack {
-            Button(action: {
-                self.whoIsThereVisible = true
-            }) {
-                
-                Text(/*@START_MENU_TOKEN@*/"Knock Knock.."/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(Color.red)
-            }
-            .alert(isPresented: $whoIsThereVisible){ () ->
-                Alert in
-                return Alert(title: Text("Who's There?" ), message: Text("This is my knock konk joke, I hope you had a lil laugh if not... alls well"), dismissButton: .default(Text("Dismiss")))
-            }
-            VStack {
-                
-                Text("BullsEye")
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.green)
-                Button(action: {
-                    print("Button pressed" )
-                    self.alertIsVisible = true
-                    })
-                {
-                    Text(/*@START_MENU_TOKEN@*/"Hit me!"/*@END_MENU_TOKEN@*/)
-                }
-                .alert(isPresented: $alertIsVisible) { () ->
-                    Alert in
-                    return Alert(title: Text("Hello there!" ), message: Text("This is my first popup"), dismissButton: .default(Text("Awesome!")))
-                }
-              
-                          
-            }
-        }
-     }
- }
+
+  @State var alertIsVisible: Bool = false
+ 
+  var body: some View {
+    VStack {
+      Text("Welcome to my first app!")
+        .fontWeight(.semibold)
+        .foregroundColor(Color.red)
+      Button(action: {
+        print("Button pressed!")
+        self.alertIsVisible = true
+      }) {
+        Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+      }
+      .alert(isPresented: $alertIsVisible) { () -> Alert in
+        return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up."), dismissButton: .default(Text("Awesome!")))
+      }
+    }
+  }
+}
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView().previewLayout(.fixed(width: 896, height: 414))
+  }
 }
