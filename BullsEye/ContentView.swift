5d03956188a72d9f481ad2476aa96d3466d251dc
Wes-White <wesleyrwhite86@gmail.com>
2020-02-18 13:10:54 -1000

    Adding more views

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -13,19 +13,57 @@ struct ContentView: View {
   @State var alertIsVisible: Bool = false
  
   var body: some View {
-    VStack {
-      Text("Welcome to my first app!")
-        .fontWeight(.semibold)
-        .foregroundColor(Color.red)
-      Button(action: {
-        print("Button pressed!")
-        self.alertIsVisible = true
-      }) {
-        Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
-      }
-      .alert(isPresented: $alertIsVisible) { () -> Alert in
-        return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up."), dismissButton: .default(Text("Awesome!")))
-      }
+    HStack {
+        HStack {
+            VStack {
+                // Target Row
+                HStack {
+                    Text("Put the bullseye as close as you can to:" )
+                    Text("100")
+            
+                }
+                
+                //Slider
+                HStack{
+                    Text("1")
+                    Slider(value: .constant(5))
+                    Text("100")
+                }
+                
+                
+                
+                //Button Row
+              Button(action: {
+                print("Button pressed!")
+                self.alertIsVisible = true
+              }) {
+                Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+              }
+                
+              .alert(isPresented: $alertIsVisible) { () -> Alert in
+                return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up."), dismissButton: .default(Text("Awesome!")))
+              }
+                
+                //Score Row
+                HStack{
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Start Over")
+                    }
+                    Text("Score:")
+                    Text("9999")
+                    Text("Round:")
+                    Text("999")
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("info")
+                    }
+
+                }
+                
+            }
+            
+        }
+        
     }
   }
 }
